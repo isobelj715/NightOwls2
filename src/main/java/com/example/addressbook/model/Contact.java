8a032938e44f2a51c5fd1406a1c1ee1dea07403f
Dan Tran <d30.tran@qut.edu.au>
2024-04-14 23:12:01 +1000

    Add Javadoc generation and publishing steps

--- src/main/java/com/example/addressbook/model/Contact.java
+++ src/main/java/com/example/addressbook/model/Contact.java
@@ -1,12 +1,20 @@
 package com.example.addressbook.model;
-
+/**
+ * A simple model class representing a contact with a first name, last name, email, and phone number.
+ */
 public class Contact {
     private int id;
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
-
+    /**
+     * Constructs a new Contact with the specified first name, last name, email, and phone number.
+     * @param firstName The first name of the contact
+     * @param lastName The last name of the contact
+     * @param email The email of the contact
+     * @param phone The phone number of the contact
+     */
     public Contact(String firstName, String lastName, String email, String phone) {
         this.firstName = firstName;
         this.lastName = lastName;
